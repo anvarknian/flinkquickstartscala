5083729d06ec98c4b5b6d9334e3972c123ebe5a8
Anwar Knyane <arsenic69@outlook.com>
2018-02-07 19:21:12 +0300

    New

--- src/main/scala/AggregationPilote.scala
+++ src/main/scala/AggregationPilote.scala
@@ -11,7 +11,7 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08
 
 
-object AggregationPilote {
+object AggregationPilote extends App  {
 
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment()
