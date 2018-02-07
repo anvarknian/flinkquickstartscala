03998f4fe95b2bcb63fea5e16c39719c0cdb9438
Anwar Knyane <arsenic69@outlook.com>
2018-02-07 16:55:26 +0300

    up

--- src/main/scala/AggregationPilote.scala
+++ src/main/scala/AggregationPilote.scala
@@ -4,11 +4,12 @@ package org.apache.flink
 
 import java.util.Properties
 
-import flink.ConfluentAvroDeserializationSchema
+import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08
+
 
 object FlinkKafkaExample extends App {
 
@@ -23,21 +24,21 @@ object FlinkKafkaExample extends App {
     val groupId = parameters.get("group-id", "flink-kafka-consumer")
     val propertiesFile = parameters.getRequired("env")
     val envProperties = ParameterTool.fromPropertiesFile(propertiesFile)
-    val schemaRegistryUrl = envProperties.getRequired("registry_url")
     val boostrapServers = envProperties.getRequired("brokers")
     val zookeeperConnect = envProperties.getRequired("zookeeper")
 
     // setup Kafka sink
-    val deserSchema = new ConfluentAvroDeserializationSchema(schemaRegistryUrl)
 
     val kafkaProps = new Properties()
     kafkaProps.setProperty("bootstrap.servers", boostrapServers)
     kafkaProps.setProperty("zookeeper.connect", zookeeperConnect)
     kafkaProps.setProperty("group.id", groupId)
 
+    val consumer = new FlinkKafkaConsumer08[String] (java.util.regex.Pattern.compile("day-[1-3]"),
+      new SimpleStringSchema,
+      kafkaProps)
 
-    val consumer = new FlinkKafkaConsumer08[String]( topic , deserSchema , kafkaProps)
-    consumer.setStartFromGroupOffsets()  // the default behaviour
+
 
     val stream = env.addSource(consumer)
 
@@ -45,19 +46,20 @@ object FlinkKafkaExample extends App {
     val windowStep = 1 // window slides 1 day
 
     val reducedStream = stream
+      .groupBy("InternalEvent")// that groups transactions in the same group
       .map(transaction => {
         transaction.numberOfTransactions = 1
         transaction
       })
-      .groupBy("InternalEvent")// that groups transactions in the same group
+
       .timeWindow(Time.days(windowSize),Time.days(windowStep))
       .sum("numberOfTransactions");
 
 
-  // Exporting result as a Gson / scala string template
+    // Exporting result as a Gson / scala string template
 
-    val streamFormatedAsJson = reducedStream.map(functionToParseDataAsJson)
-    streamFormatedAsJson.sink(SinkToWriteYourData)
+    // val streamFormatedAsJson = reducedStream.map(functionToParseDataAsJson)
+    // streamFormatedAsJson.sink(SinkToWriteYourData)
 
 
   }
